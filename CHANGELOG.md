cf30884ea2d82d97292917619805e3b37e4435db
Falk <gammalget@gmail.com>
2019-04-21 22:06:34 +0200

    cleanup in repo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # CHANGELOG
 
+## 2019-04-21
+
+- [CLEANUP]: A first try to clean up the repo. Moved each active branch into directories in master. [Falk]
+
+## 2019-04-19
+
+- [FEATURE]: Forked out a new bransh with postgresql as backend. [Falk]
+
 ## 2019-04-16
 
 - [README]: Updated readme with some goodtohave commands.[Falk]
---
+++ psql/script/entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -o pipefail
+
+
+# Wait for us to be able to connect to REDIS before proceeding
+echo "===> Waiting for REDIS service"
+service redis-server restart
+while [ ! -e /var/run/redis/redis-server.sock ]
+do
+  service redis-server restart
+  sleep 2
+done
+
+X="$(redis-cli -s /var/run/redis/redis-server.sock ping)"
+while  [ "${X}" != "PONG" ]; do
+        echo "Redis not yet ready..."
+        sleep 1
+        X="$(redis-cli -s /var/run/redis/redis-server.sock ping)"
+done
+
+service postgresql restart 
+
+echo "===> Checking if db is ready"
+if psql gvmd -c '\q' 2>&1; then
+   echo "database is ready"
+else
+  echo "Creating database gvmd and settings"
+  su postgres sh -c "createuser -DRS root " ;\
+  su postgres sh -c "createdb -O root gvmd" ;\
+  su postgres sh -c "psql -d gvmd"   << EOSQL
+      create role dba with superuser noinherit;
+      grant dba to root;
+      CREATE EXTENSION "uuid-ossp";
+EOSQL
+fi
+
+# Check if ssl certs are in place (it's rather late and I will fix this more elegant later[tm])
+echo "===> Waiting to get certs ready"
+until /usr/local/bin/gvm-manage-certs -V -q
+do
+  /usr/local/bin/gvm-manage-certs -af
+done
+
+# Check if admin exists, if not create admin
+if $(/usr/local/sbin/gvmd --get-users | grep -q 'admin') ; then
+    echo "---> Admin already exists.."
+else
+    echo "---> Creating admin with new password"
+    /usr/local/sbin/gvmd --create-user=admin --password=admin
+fi
+
+
+# Try to start certdata and scapdata sync
+echo "---> Starting Certsync.." ;\
+/usr/local/sbin/greenbone-certdata-sync ;\
+echo "---> Starting Scapsync.." ;\
+/usr/local/sbin/greenbone-scapdata-sync
+
+
+# Start GVM stuffs
+echo "---> Starting OPENVASSD"
+openvassd
+echo "---> Starting GVMD"
+gvmd 
+echo "---> Starting GSAD"
+gsad 
+
+
+# WHATTODOWITTHIS?
+if [ -z "$BUILD" ]; then
+  echo "Tailing logs"
+  tail -F /usr/local/var/log/gvm/*
+fi
